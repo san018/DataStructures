6f38be54095035c6bc774547efa6c237514dcb59
Sangeetha Sundar <thestrongbattles@gmail.com>
2018-06-26 15:49:03 +0530

    Update LevelOrderTraversal.cpp

--- Trees/LevelOrderTraversal.cpp
+++ Trees/LevelOrderTraversal.cpp
@@ -3,7 +3,6 @@
 Level-Order Traversal : all nodes in level 1 to n
 Use a deque to put the root, left and the right child respectively and then we deque one by one.
 
-*/
 
 
 You are given a pointer to the root of a binary tree. You need to print the level order traversal of this tree. In level order traversal, we visit the nodes level by level from left to right. You only have to complete the function. For example:
@@ -54,63 +53,6 @@ Explanation
 We need to print the nodes level by level. We process each level from left to right. 
 Level Order Traversal: 1 -> 2 -> 5 -> 3 -> 6 -> 4.
 
-
-*/
-
-
-
-
-You are given a pointer to the root of a binary tree. You need to print the level order traversal of this tree. In level order traversal, we visit the nodes level by level from left to right. You only have to complete the function. For example:
-
-     1
-      \
-       2
-        \
-         5
-        /  \
-       3    6
-        \
-         4  
-For the above tree, the level order traversal is 1 -> 2 -> 5 -> 3 -> 6 -> 4.
-
-Input Format
-
-You are given a function,
-
-void levelOrder(node * root) {
-
-}
-Constraints
-
-1 Nodes in the tree  500
-
-Output Format
-
-Print the values in a single line separated by a space.
-
-Sample Input
-
-     1
-      \
-       2
-        \
-         5
-        /  \
-       3    6
-        \
-         4  
-Sample Output
-
-1 2 5 3 6 4
-
-Explanation
-
-We need to print the nodes level by level. We process each level from left to right. 
-Level Order Traversal: 1 -> 2 -> 5 -> 3 -> 6 -> 4.
-
-
-
-/*
 struct node
 {
     int data;
